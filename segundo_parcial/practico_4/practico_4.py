654087d5744ac9af6b2c4e5195a343e695b452e4
Diegoolei <Diegoolei@github.com>
2022-06-22 17:24:23 -0300

    Ejercicio 3 p4

--- segundo_parcial/practico_4/practico_4.py
+++ segundo_parcial/practico_4/practico_4.py
@@ -1,11 +1,13 @@
 from random import random
-from math import exp, factorial, comb
+from math import exp, factorial, comb, sqrt
 import time
 import numpy as np
 
 N_sim = 10000
 
 #!NOTE Funciones generales
+
+
 def uniforme_d(n, m):
     """
     Genera numeros aleatorios entre n y m (inclusive) 
@@ -30,6 +32,30 @@ def permutar(a):
     return a
 
 
+def estimar_esperanza(generador, N_muestras):
+    suma = 0
+    for _ in range(N_muestras):
+        suma += generador()
+    esperanza = suma/N_muestras
+    return esperanza
+
+
+def estimar_varianza(generador, N_muestras):
+    """
+    Estima el Varianza de una variable aleatoria el estimador de la varianza Sn
+    Args:
+        M: Cantidad de valores a promediar (tamaño de la muestra)
+    """
+    MU = estimar_esperanza(generador, N_muestras)
+    varianza = 0
+    for _ in range(N_muestras):
+        varianza += (generador() - MU) ** 2
+
+    varianza = varianza / (N_muestras - 1)
+    return varianza
+
+
+#!NOTE Auxiliares ejercicio 1
 def simulacion_1_i(r, iteraciones):
     """
     probabilidad de que  las primeras r cartas sean coincidencias
@@ -132,7 +158,7 @@ def ej_1():
     esperanza_estimada = np.round(esperanza_1(10**4, 100), 2)
     varianza_estimada = np.round(varianza_1(10**4, 100), 2)
     r = 10
-    
+
     print("------------EJERCICIO 1-------------")
     print("Esperanza estimada =", esperanza_estimada,
           "\nVarianza estimada =", varianza_estimada)
@@ -150,8 +176,8 @@ def ej_1():
             "Con {10**i} iteraciones la probabilidad es {simulacion_1_ii(r, 10**i)}")
 
 
-#! Auxiliares ejercicio 2
-def sim_2(M,N):
+#!NOTE Auxiliares ejercicio 2
+def sim_2(M, N):
     """
     Aproxima la Sumatoria k=1 a N de exp(k/N) usando M valores
     """
@@ -161,7 +187,7 @@ def sim_2(M,N):
     for _ in range(M):
         U = uniforme_d(1, N)
         suma += exp(U/N)
-    suma = suma*(N/M) #! Aproximo de 1 a N usando solo 100 valores   
+    suma = suma*(N/M)  # ! Aproximo de 1 a N usando solo 100 valores
     # stop = time.time() # Tiempo de ejecución
     return suma
 
@@ -195,9 +221,40 @@ def ej_2():
     print(f"Valor real:     {round(exacta, 4)}")
 
 
-#! Auxiliares ejercicio 3
+#!NOTE Auxiliares ejercicio 3
 def sim_3():
-    while True:
+    """
+    Genero dos valores aleatorios entre 1 y 6 inclusive, si el
+    valor no se había visto lo guardo un arreglo. Todos los ya 
+    vistos estarán en ese arreglo. Aumento el contador y repito
+    hasta tener 11 elementos distintos en el arreglo y devuelvo
+    el contador.
+    """
+    resultados_vistos = []
+    resultados_distintos = 0
+    N = 0
+    while resultados_distintos != 11:
+        dado_1, dado_2 = uniforme_d(1, 6), uniforme_d(1, 6)
+        resultado = dado_1 + dado_2
+        if (not resultado in resultados_vistos):
+            resultados_vistos.append(resultado)
+            resultados_distintos += 1
+        N += 1
+    return N
+
+
+def probabilidad_al_menos_X(X, iteraciones):
+    """
+    Estima la probabilidad de que la variable aleatoria N sea mayor o igual a X
+    Args:
+        X:            Valor minimo de N
+        iteraciones : Cantidad de iteraciones del algoritmo
+    """
+    estimacion = 0
+    for _ in range(iteraciones):
+        if (sim_3() >= X):
+            estimacion += 1
+    return estimacion / iteraciones
 
 
 #!NOTE Ejercicio 3
@@ -205,9 +262,37 @@ def ej_3():
     """
     Simulación tirada de dados
     """
-    pass
+    #! B)
+    esperanza = []
+    varianza = []
+    probabilidad_al_menos_15 = []
+    probabilidad_a_lo_sumo_9 = []
+    for i in range(1, 3):
+        #! i)
+        esperanza.append(estimar_esperanza(sim_3, 100**i))
+        varianza.append(sqrt(estimar_varianza(sim_3, 100**i)))
+        
+        
+    for i in range(1, 3):
+        #! ii)
+    
+        probabilidad_al_menos_15.append(probabilidad_al_menos_X(15, 100**i))
+        probabilidad_a_lo_sumo_9.append(1 - probabilidad_al_menos_X(9, 100**i))
 
+    print("\nEsperanza estimada:")
+    print(
+        f"simulaciones:valor, 100:{esperanza[0]}, 1000:{esperanza[1]} ")
 
+    print("\ndesviacion estandar estimada:")
+    print(
+        f"simulaciones:valor, 100:{varianza[0]}, 1000:{varianza[1]} ")
 
-#ej_1()
-#ej_2()
+    print("\nProbabilidad al menos 15:")
+    print(f"simulaciones:valor, 100:{probabilidad_al_menos_15[0]}, 1000:{probabilidad_al_menos_15[1]}")
+
+    print("\nProbabilidad a lo sumo 9:")
+    print(f"simulaciones:valor, 100:{probabilidad_a_lo_sumo_9[0]}, 1000:{probabilidad_a_lo_sumo_9[1]}")
+
+# ej_1()
+# ej_2()
+# ej_3()
